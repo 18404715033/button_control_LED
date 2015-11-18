871196df485ad0e31cfa01e40a78a0f96c97a2e0
18404715033 <965630447@qq.com>
2015-11-18 21:08:07 +0800

    按键控制led第二次
    增加了纪录led状态 boolean ledState=false;
    和纪录按键状态 boolean buttonState=true;
    还加入了等待按键按下 while(digitalRead(buttonPin)==HIGH){}的循环
    更改了点亮led的方式if(buttonState==true)
      {
        digitalWrite(ledPin,LOW);
        ledState=!ledState;
      }
      else
      {
        digitalWrite(ledPin,HIGH);
        ledState=!ledState;
      }
      delay(500);

--- button_control_LED.ino
+++ button_control_LED.ino
@@ -1,6 +1,8 @@
 int buttonPin = 2;
 int ledPin = 13;
-int buttonState = 0;
+boolean ledState=false;//记录LED状态
+boolean buttonState=true;//记录按键状态
+//int buttonState = 0;
 void setup() 
 {
   // put your setup code here, to run once:
@@ -12,15 +14,20 @@ void setup()
 void loop() 
 {
   // put your main code here, to run repeatedly:
-  buttonState = digitalRead(buttonPin);
+  //buttonState = digitalRead(buttonPin);
   //按住按键,点亮LED;放开按键后,熄灭LED;
-  if(buttonState==HIGH)
+  while(digitalRead(buttonPin)==HIGH){}//等待按键按下
+  //当按键按下时,点亮或熄灭LED
+  if(buttonState==true)
   {
     digitalWrite(ledPin,LOW);
+    ledState=!ledState;
   }
   else
   {
     digitalWrite(ledPin,HIGH);
+    ledState=!ledState;
   }
+  delay(500);
 
 }
